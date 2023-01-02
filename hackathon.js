81c8102e9227149360288e638ee8675b0b965b8a
Karan <karanpargal007@gmail.com>
2023-01-02 21:29:09 +0530

    :sparkles:Add prize section

--- hackathon.js
+++ hackathon.js
@@ -1,5 +1,57 @@
-const prizeSection = document.getElementById("prizes");
+const prizeSection = document.getElementById("content");
+const polygonBox = document.getElementById("polygon");
+const replitBox = document.getElementById("replit");
+const filecoinBox = document.getElementById("filecoin");
+const solanaBox = document.getElementById("solana");
 
-prizeSection.innerHTML = `
-<div class="bg-green-400 text-6xl">Lets hack</div>
-`;
+
+function polygon(){
+
+    prizeSection.innerHTML = `
+    <h1 class="text-2xl font-bold mb-6">Polygon</h1>
+            <p class="mb-4"><span class="text-[#3770FF] font-medium">Polygon</span> is a protocol and a framework for building and connecting Ethereum-compatible blockchain networks.</p>
+            <p class="mb-3">Prizes up for grabs:</p>
+            <p class="mb-3 ml-12">₹10,000 for the best hack built on Ethereum, or</p>
+            <p class="mb-2 ml-12">₹15,000 for the best hack built on Ethereum + Polygon,</p>
+            <p class="mb-3 ml-12">Eligibility to apply for internship/full-time roles and seed funding of up to 5,000 USD for winners!</p>
+            <p class="mb-3">Read about the bounty details and find code templates for Polygon here:</p>
+            <a href="https://nsb.dev/polygon-bounty" class="mb-3 text-[#3770FF] font-medium">https://nsb.dev/polygon-bounty.</p>
+    `;
+}
+
+function replit(){
+    prizeSection.innerHTML = `
+    <h1 class="text-2xl font-bold mb-6">Replit</h1>
+            <p class="mb-4"><span class="text-[#3770FF] font-medium">Polygon</span> is a protocol and a framework for building and connecting Ethereum-compatible blockchain networks.</p>
+            <p class="mb-3">Prizes up for grabs:</p>
+            <p class="mb-3 ml-12">₹10,000 for the best hack built on Ethereum, or</p>
+            <p class="mb-2 ml-12">₹15,000 for the best hack built on Ethereum + Polygon,</p>
+            <p class="mb-3 ml-12">Eligibility to apply for internship/full-time roles and seed funding of up to 5,000 USD for winners!</p>
+            <p class="mb-3">Read about the bounty details and find code templates for Polygon here:</p>
+            <a href="https://nsb.dev/polygon-bounty" class="mb-3 text-[#3770FF] font-medium">https://nsb.dev/polygon-bounty.</p>
+    `;
+}
+function filecoin(){
+    prizeSection.innerHTML = `
+    <h1 class="text-2xl font-bold mb-6">Filecoin</h1>
+            <p class="mb-4"><span class="text-[#3770FF] font-medium">Polygon</span> is a protocol and a framework for building and connecting Ethereum-compatible blockchain networks.</p>
+            <p class="mb-3">Prizes up for grabs:</p>
+            <p class="mb-3 ml-12">₹10,000 for the best hack built on Ethereum, or</p>
+            <p class="mb-2 ml-12">₹15,000 for the best hack built on Ethereum + Polygon,</p>
+            <p class="mb-3 ml-12">Eligibility to apply for internship/full-time roles and seed funding of up to 5,000 USD for winners!</p>
+            <p class="mb-3">Read about the bounty details and find code templates for Polygon here:</p>
+            <a href="https://nsb.dev/polygon-bounty" class="mb-3 text-[#3770FF] font-medium">https://nsb.dev/polygon-bounty.</p>
+    `;
+}
+function solana(){
+    prizeSection.innerHTML = `
+    <h1 class="text-2xl font-bold mb-6">Solana</h1>
+            <p class="mb-4"><span class="text-[#3770FF] font-medium">Polygon</span> is a protocol and a framework for building and connecting Ethereum-compatible blockchain networks.</p>
+            <p class="mb-3">Prizes up for grabs:</p>
+            <p class="mb-3 ml-12">₹10,000 for the best hack built on Ethereum, or</p>
+            <p class="mb-2 ml-12">₹15,000 for the best hack built on Ethereum + Polygon,</p>
+            <p class="mb-3 ml-12">Eligibility to apply for internship/full-time roles and seed funding of up to 5,000 USD for winners!</p>
+            <p class="mb-3">Read about the bounty details and find code templates for Polygon here:</p>
+            <a href="https://nsb.dev/polygon-bounty" class="mb-3 text-[#3770FF] font-medium">https://nsb.dev/polygon-bounty.</p>
+    `;
+}
