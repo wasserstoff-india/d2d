c8c0489566a6490887aec58e4de0e3625ff169c9
Karan <karanpargal007@gmail.com>
2023-01-03 00:01:11 +0530

    Add d2d prizes

--- hackathon.js
+++ hackathon.js
@@ -3,6 +3,7 @@ const polygonBox = document.getElementById("polygon");
 const replitBox = document.getElementById("replit");
 const filecoinBox = document.getElementById("filecoin");
 const solanaBox = document.getElementById("solana");
+const d2dBox = document.getElementById("d2d");
 
 
 function polygon(){
@@ -10,6 +11,7 @@ function polygon(){
     replitBox.style.backgroundColor = "transparent";
     filecoinBox.style.backgroundColor = "transparent";
     solanaBox.style.backgroundColor = "transparent";
+    d2dBox.style.backgroundColor = "transparent";
     prizeSection.innerHTML = `
     <h1 class="text-2xl font-bold mb-6">Polygon</h1>
             <p class="mb-4"><span class="text-[#3770FF] font-medium">Polygon</span> is a protocol and a framework for building and connecting Ethereum-compatible blockchain networks.</p>
@@ -27,6 +29,7 @@ function replit(){
     replitBox.style.backgroundColor = "#E45A2E";
     filecoinBox.style.backgroundColor = "transparent";
     solanaBox.style.backgroundColor = "transparent";
+    d2dBox.style.backgroundColor = "transparent";
     prizeSection.innerHTML = `
     <h1 class="text-2xl font-bold mb-6">Replit</h1>
             <p class="mb-4"><span class="text-[#3770FF] font-medium">Polygon</span> is a protocol and a framework for building and connecting Ethereum-compatible blockchain networks.</p>
@@ -43,6 +46,7 @@ function filecoin(){
     replitBox.style.backgroundColor = "transparent";
     filecoinBox.style.backgroundColor = "#E45A2E";
     solanaBox.style.backgroundColor = "transparent";
+    d2dBox.style.backgroundColor = "transparent";
     prizeSection.innerHTML = `
     <h1 class="text-2xl font-bold mb-6">Filecoin</h1>
             <p class="mb-4"><span class="text-[#3770FF] font-medium">Polygon</span> is a protocol and a framework for building and connecting Ethereum-compatible blockchain networks.</p>
@@ -59,6 +63,25 @@ function solana(){
     replitBox.style.backgroundColor = "transparent";
     filecoinBox.style.backgroundColor = "transparent";
     solanaBox.style.backgroundColor = "#E45A2E";
+    d2dBox.style.backgroundColor = "transparent";
+    prizeSection.innerHTML = `
+    <h1 class="text-2xl font-bold mb-6">Solana</h1>
+            <p class="mb-4"><span class="text-[#3770FF] font-medium">Polygon</span> is a protocol and a framework for building and connecting Ethereum-compatible blockchain networks.</p>
+            <p class="mb-3">Prizes up for grabs:</p>
+            <p class="mb-3 ml-12">₹10,000 for the best hack built on Ethereum, or</p>
+            <p class="mb-2 ml-12">₹15,000 for the best hack built on Ethereum + Polygon,</p>
+            <p class="mb-3 ml-12">Eligibility to apply for internship/full-time roles and seed funding of up to 5,000 USD for winners!</p>
+            <p class="mb-3">Read about the bounty details and find code templates for Polygon here:</p>
+            <a href="https://nsb.dev/polygon-bounty" class="mb-3 text-[#3770FF] font-medium">https://nsb.dev/polygon-bounty.</p>
+    `;
+}
+
+function d2d(){
+    polygonBox.style.backgroundColor = "transparent";
+    replitBox.style.backgroundColor = "transparent";
+    filecoinBox.style.backgroundColor = "transparent";
+    solanaBox.style.backgroundColor = "transparent";
+    d2dBox.style.backgroundColor = "#E45A2E";
     prizeSection.innerHTML = `
     <h1 class="text-2xl font-bold mb-6">Solana</h1>
             <p class="mb-4"><span class="text-[#3770FF] font-medium">Polygon</span> is a protocol and a framework for building and connecting Ethereum-compatible blockchain networks.</p>
