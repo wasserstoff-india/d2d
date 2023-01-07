36e4da942119e6a21f494cf6d2cff117828f8ad0
Anubhav Pathak <anubhavpathak99@gmail.com>
2023-01-08 01:11:52 +0530

    Added About Section

--- Home.js
+++ Home.js
@@ -1,28 +1,29 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js'
 
+const toggler = document.getElementById("navbar-toggler");
+const menu = document.getElementById("navlist");
+toggler.addEventListener("click", () => {
+  toggler.classList.toggle("active");
+  menu.style.display = (toggler.classList.contains("active")) ? "block" : "none";
+});
+
 const swiper = new Swiper('.swiper', {
     loop: true,
     autoplay: {
         delay: 1000,
         disableOnInteraction: false,
     },
-    // If we need pagination
     pagination: {
         el: ".swiper-pagination",
         dynamicBullets: true,
     },
-
-    // Default parameters
     slidesPerView: 1,
     spaceBetween: 10,
-    // Responsive breakpoints
     breakpoints: {
-      // when window width is >= 320px
       280: {
         slidesPerView: 2,
         spaceBetween: 20
       },
-      // when window width is >= 480px
       480: {
         slidesPerView: 4,
         spaceBetween: 30
@@ -36,4 +37,15 @@ const swiper = new Swiper('.swiper', {
         spaceBetween: 40
       }
     }
-  })
+  });
+
+  const swiperAbout = new Swiper('.swiper-about', {
+    speed: 300,
+    loop: true,
+    allowTouchMove: false,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    slidesPerView: 1,
+  });
